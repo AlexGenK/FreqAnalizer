8a07a8c771857cc0bc5a0344181410a54162ee93
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-06-22 23:43:19 +0300

    sceleton for loading code

--- start.rb
+++ start.rb
@@ -1,3 +1,6 @@
+# encoding: utf-8
+$LOAD_PATH << './exercises/'
+
 ar_exer=['Frequency analyzer',
          'Reverce of the sentence']
 
@@ -5,4 +8,9 @@ puts
 ar_exer.each_with_index {|item, index| puts "#{index} - #{item}"}
 puts
 print "Select exercice: "
-n=gets
+case gets.to_i
+  when 0 
+    require "fran.rb"
+  else
+    print "Incorrect input!"
+end
