f478f0505f13e000ff8ac1b72297e9b8cd709822
Oleksii Kozhyn <suburbakskin@gmail.com>
2016-06-28 21:49:57 +0300

    add more comments

--- start.rb
+++ start.rb
@@ -1,15 +1,24 @@
 # encoding: utf-8
 
+# сборник задач для программистов
+
+# гем, обеспечивающий вывод в консоли в цвете
 require 'colorize'
+
+# путь загрузки модулей. один модуль - одна задача
 $LOAD_PATH << './exercises/'
 
+# список задач
 ar_exer=['Frequency analyzer',
          'Reverce of the sentence']
 
+# вывод списка задач и выбор нужной задачи
 puts "\n"+"-"*20+" Exercises "+"-"*20
 ar_exer.each_with_index {|item, index| puts "#{index} - #{item}"}
 puts "-"*51+"\n\n"
 print "Select exercice: "
+
+# запуск соответствующего модуля или вывод сообщения об ошибке
 case gets.chomp
   when "0" 
     require "fran.rb"
